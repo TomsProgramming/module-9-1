04738a655c52a1a27a6a782948d91ff5d062da7c
Tom Tiedemann <tomtiedemann30@gmail.com>
2026-09-09 12:02:05 +0200

    bestanden gekopieert vanaf opdracht 1 (Ik weet dat ik ook ene nieuwe branch kon aanmaken vanaf de opdracht 1 branch)

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel From Scratch Blog</title>
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body style="font-family: Open Sans, sans-serif">
+    {{ $slot }}
+</body>
+</html>
